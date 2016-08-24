8249904cdc2ba89570bdc7346bf577531c197db7
Hernan Y.Ke <yuriqiao@gmail.com>
2016-08-24 19:57:28 -0400

    start basic window

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-main.js
+const electron = require('electron');
+
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow
+
+app.on('ready',_=>{
+	new BrowserWindow({
+		height:200,
+		width:200
+	})
+})
