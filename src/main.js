bb3de6b61919b388aeeaed2db342fa4baa033dad
Hernan Y.Ke <yuriqiao@gmail.com>
2016-08-24 20:09:51 -0400

    loadURL(``) to load webpage

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow
 
+let mainWindow
 app.on('ready',_=>{
-	new BrowserWindow({
+	mainWindow = new BrowserWindow({
 		height:200,
 		width:200
+	})
+
+	mainWindow.loadURL(`file://${__dirname}/ke.html`)
+
+	mainWindow.on('closed', _=>{
+		mainWindow = null;
 	})
 })
